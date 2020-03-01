818db28ba85dac8b08d58bd6f5f3f586865737ee
manishdangi98 <manish1998dangi@gmail.com>
2020-03-01 11:22:23 +0530

    heroku setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log(__dirame + "/../public");
+const path = require('path');
+const express = require('express');
+
+// console.log(__dirname + "/../public");
+// console.log(path.join(__dirname, '/../public'));
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}`);
+})
