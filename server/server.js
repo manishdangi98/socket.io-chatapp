c60bffe5bf6951bffe02c09b32b7555c0ff4801e
manishdangi98 <manish1998dangi@gmail.com>
2020-03-14 17:49:28 +0530

    admin msg for new user

--- server/server.js
+++ server/server.js
@@ -19,15 +19,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
     console.log('New user just connected');
 
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAT : new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user Joined',
+        createdAT : new Date().getTime()
+    });
+
     
     socket.on('createMessage', (message) => {
         console.log("createMessage", message);
         
-        io.emit('newMessage',{
-            from: message.from,
-            text: message.text,
-            createdAT: new Date().getTime(),
-        })
+        // io.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAT: new Date().getTime(),
+        // })
+        socket.broadcast.emit('newMessage',{
+                from: message.from,
+                text: message.text,
+                createdAT: new Date().getTime(),
+            })
     })
 
     socket.on('disconnect', () =>{
